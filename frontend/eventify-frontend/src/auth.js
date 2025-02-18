4a511350cfddd062dc624cdc6a2950826971a27b
Johny-Robit <jean.robitaille.2@ulaval.ca>
2025-02-18 01:05:46 -0500

    Removed fronted from Heroku deployment

--- frontend/eventify-frontend/src/auth.js
+++
@@ -1,9 +0,0 @@
-import { ref } from 'vue'
-
-export const isLoggedIn = ref(false)
-
-export const user = ref({
-  username: 'JohnDoe420',
-  email: 'john.doe@example.com',
-  password: 'password123'
-})
