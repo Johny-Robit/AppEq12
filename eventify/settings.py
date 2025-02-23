44b5be565d1054c6d8dca1f8f3554b247b956dd2
Johny-Robit <jean.robitaille.2@ulaval.ca>
2025-02-23 18:22:31 -0500

    Added csrf view middleware

--- eventify/settings.py
+++ eventify/settings.py
@@ -62,10 +62,11 @@ AUTH_USER_MODEL = "events.CustomUser"
 
 # Middleware
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
