8ee430b5d3f5c1cd340b8bead038d08124794dcd
Simon-Pierre Vachon <simon-pierre.vachon.2@ulaval.ca>
2025-04-07 16:59:03 -0400

    updated with the backend from backend-localhost

---
+++ eventify/settings.py
@@ -0,0 +1,271 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
+set_environment = 'localhost' # Change this to 'heroku' when deploying to Heroku
+
+# Base directory
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+
+
+# Sécurité
+SECRET_KEY = os.getenv('SECRET_KEY')
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# configuration par environnement
+if set_environment == 'localhost':
+    DEBUG = True
+
+    # Domaine du serveur django
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+        #'http://localhost:5173',
+        'http://localhost:8000',
+    ]
+
+    # Origines autorisés à faire des requêtes au backend
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173",
+        #"http://localhost:8000",
+    ]
+
+    # Origines de confiance pouvant faire des requêtes Post, put, delete, patch
+    # Domaine du serveur frontend et Domaine du backend qui peut se faire des requêtes à lui-même
+    CSRF_TRUSTED_ORIGINS = [
+        "http://localhost:5173",
+        "http://localhost:8000",
+    ]
+    
+elif set_environment == 'heroku':
+    DEBUG = False
+
+    # Domaine du serveur django
+    ALLOWED_HOSTS = [
+        'app-eq-12-eventify-29bf10cbb7c2.herokuapp.com',
+        #'johny-robit.github.io',
+    ]
+
+    # Origines autorisés à faire des requêtes au backend
+    CORS_ALLOWED_ORIGINS = [
+        "https://johny-robit.github.io",
+        #"https://app-eq-12-eventify-29bf10cbb7c2.herokuapp.com"
+    ]
+
+    # Origines de confiance pouvant faire des requêtes Post, put, delete, patch
+    # Domaine du serveur frontend et Domaine du backend qui peut se faire des requêtes à lui-même
+    CSRF_TRUSTED_ORIGINS = [
+        "https://johny-robit.github.io",
+        "https://app-eq-12-eventify-29bf10cbb7c2.herokuapp.com",
+    ]
+
+
+# CORS Configuration commune (localhost & heroku)
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = False  # Désactiver si on veut utiliser CORS_ALLOWED_ORIGINS
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+]
+
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+    "x-requested-with",
+    "accept",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+
+# Applications Django
+INSTALLED_APPS = [
+    'corsheaders',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Packages
+    'rest_framework',
+
+    # Application interne
+    'events'
+]
+
+# Le fichier racine pour les urls
+ROOT_URLCONF = 'eventify.urls'
+
+# Authentification sur mesure avec token stocké en base
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "events.authentication.TokenAuthentication",
+    ],
+}
+
+ACCESS_TOKEN_LIFETIME = 30  # minutes
+REFRESH_TOKEN_LIFETIME = 7  # jours
+AUTH_USER_MODEL = "events.CustomUser"  # Modèle d'utilisateur personnalisé
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+
+# Paramètrage pour la validation de passwords
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+WSGI_APPLICATION = 'eventify.wsgi.application'
+
+
+# Paramètres divers
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Pour pytest-django
+pytest_plugins = ["django.contrib.auth"]
+
+# Logging
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+        'eventify_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'eventify.log'),
+            'maxBytes': 1024 * 1024 * 5,
+            'backupCount': 3,
+            'formatter': 'verbose',
+        },
+        'events_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOG_DIR, 'events.log'),
+            'maxBytes': 1024 * 1024 * 5,  
+            'backupCount': 3,  
+            'formatter': 'verbose',
+        },
+        #'django_file': {  # DEBUG Nouveau handler pour Django
+        #    'level': 'DEBUG',
+        #    'class': 'logging.FileHandler',
+        #    'filename': os.path.join(LOG_DIR, 'debug.log'),
+        #    'formatter': 'verbose',
+        #},
+    },
+    'loggers': {
+        'eventify': {
+            'handlers': ['console', 'eventify_file'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'events': {
+            'handlers': ['console', 'events_file'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        #'django': {  # DEBUG Nouveau logger pour Django
+        #    'handlers': ['console', 'django_file'],
+        #    'level': 'DEBUG',
+        #    'propagate': True,
+        #},
+    },
+}
+
+
+
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
---
+++ events/authentication.py
@@ -0,0 +1,22 @@
+from rest_framework.authentication import BaseAuthentication
+from .models import AccessToken
+from datetime import datetime
+from django.utils import timezone
+
+class TokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get("Authorization")
+        if not token:
+            return None  # Pas de token = pas d'authentification
+
+        token = token.replace("Bearer ", "")  # Enlever "Bearer "
+
+        try:
+            access_token = AccessToken.objects.get(token=token)
+
+            if access_token.expires_at < timezone.now():
+                return None # Token expiré
+
+            return (access_token.user, None)
+        except AccessToken.DoesNotExist:
+            return None
