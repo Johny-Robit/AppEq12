09722ea7e02b27cb736cd077dd12d7451a4f181d
Johny-Robit <jean.robitaille.2@ulaval.ca>
2025-04-06 20:16:11 -0400

    removed lambda function

--- events/models.py
+++ events/models.py
@@ -4,16 +4,21 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from datetime import datetime, timedelta
 import uuid
+from django.utils.timezone import now
 
+def default_expires_at():
+    return now() + timedelta(minutes=30)
+
+def default_refresh_expires_at():
+    return now() + timedelta(days=7)
 
 class AccessToken(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     token = models.UUIDField(default=uuid.uuid4, unique=True)
     refresh_token = models.UUIDField(default=uuid.uuid4, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(default=lambda: datetime.now() + timedelta(minutes=30))
-    created_at = models.DateTimeField(auto_now_add=True)
-    refresh_expires_at = models.DateTimeField(default=lambda: datetime.now() + timedelta(days=7))
+    expires_at = models.DateTimeField(default=default_expires_at)
+    refresh_expires_at = models.DateTimeField(default=default_refresh_expires_at)
 
 
 class CustomUser(AbstractUser):
