900adfee4b98ba2e0d125be5f50b83843109e747
Simon-Pierre Vachon <simon-pierre.vachon.2@ulaval.ca>
2025-04-07 22:26:30 -0400

    yup yup

--- frontend/eventify-frontend/src/main.js
+++ frontend/eventify-frontend/src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios';
 
-axios.get('http://localhost:8000/api/csrf/').then(() => {
+axios.get('https://app-eq-12-eventify-29bf10cbb7c2.herokuapp.com/api/csrf/').then(() => {
   console.log('CSRF token set');
 });
 
